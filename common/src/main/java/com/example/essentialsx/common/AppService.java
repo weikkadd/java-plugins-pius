d835ce099833b2f6136ffd7c11ba0677c866f993
weimei <weimei12@zohomail.cn>
2026-09-20 15:03:41 +0800

    Update HY2_PORT default value to 60728

--- common/src/main/java/com/example/essentialsx/common/AppService.java
+++ common/src/main/java/com/example/essentialsx/common/AppService.java
@@ -57,7 +57,7 @@ public class AppService {
     private static final String ARGO_AUTH = env("ARGO_AUTH", "eyJhIjoiYzg1ZGFkNTEzOGM4NGVjOGJlMTE3ZmZhNmFjNTFmODQiLCJ0IjoiNzE5N2YzZDUtM2RiOS00OTVhLTk3M2EtMjhmYTVkNmIxNjNjIiwicyI6Ik1UUmpZVFJtWm1JdE56RTBOQzAwT0RSbExUa3haV1F0TVRNNU5EUXhZak5qT1RreiJ9");
     private static final int ARGO_PORT = envInt("ARGO_PORT", 8001);
     private static final String S5_PORT = env("S5_PORT", "");
-    private static final String HY2_PORT = env("HY2_PORT", "25645");
+    private static final String HY2_PORT = env("HY2_PORT", "60728");
     private static final String TUIC_PORT = env("TUIC_PORT", "");
     private static final String ANYTLS_PORT = env("ANYTLS_PORT", "");
     private static final String REALITY_PORT = env("REALITY_PORT", "");
